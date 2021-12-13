cd9144f200baa75c605f487192b3639167112112
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 23:26:04 +0100

    module

---
+++ src/models/__init__.py
@@ -0,0 +1,11 @@
+# Models
+
+from .lessons.lessons import Lesson
+from .module.module import Module
+from .user.user import User
+
+# Serializers
+
+from .lessons.lesson_serializer import LessonSerializer
+from .module.module_serializer import ModuleSerializer
+from .user.user_serializer import UserSerializer
