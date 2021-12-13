a281e6023a15e225e3adb66a3fff595b0a1bc7d9
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 23:27:00 +0100

    user model

---
+++ src/models/user/user.py
@@ -0,0 +1,19 @@
+from django.db import models
+from ..base import Base
+
+class User(Base, models.Model):
+    username = models.CharField(max_length=80, blank=False, null=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, null=False)
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+        db_table = 'user'
+        ordering = ['id']
+
+        unique_together = ['email']
+
+    def __str__(self):
+        return self.username
