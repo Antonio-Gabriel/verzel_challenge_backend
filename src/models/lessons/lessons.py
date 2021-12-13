e715e47e88ec446684fced6958695680e4db5fa4
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 23:26:25 +0100

    lesson model

---
+++ src/models/lessons/lessons.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from ..module.module import Module
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    module = models.ForeignKey(Module, related_name="lessons", on_delete=models.CASCADE)
+    start_date = models.DateField()
+
+    class Meta:
+        verbose_name = "Lesson"
+        verbose_name_plural = "Lessons"
+
+        db_table = "lesson"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"O curso {self.name} pertence ao modulo {self.module}"
