70caccaaeb594b7f14edc96160323e4ba7f4822f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 23:27:14 +0100

    lesson service

---
+++ src/services/lesson_service.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions, viewsets
+
+from ..models import Lesson, LessonSerializer
+
+class LessonServiceViewSet(viewsets.ModelViewSet):
+    """ Lesson Service """
+
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+    ]
