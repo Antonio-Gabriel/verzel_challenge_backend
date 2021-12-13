f75c251140174199e36d9c53e8a6ada3ec7d3744
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-14 00:02:56 +0100

    serialize user routes

--- src/models/user/user_serializer.py
+++ src/models/user/user_serializer.py
@@ -1,15 +1,22 @@
 from .user import User
 
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     """User serializer"""
 
+    password = serializers.CharField(
+        style={"input_type": "password"}, write_only=True, label="Password"
+    )
+
     class Meta:
-        extra_kwargs = {"email": {"write_only": True}}
+        extra_kwargs = {"email": {"write_only": True}, "password": {"write_only": True}}
         model = User
-        fields = "__all__"
+        fields = ("id", "username", "password", "state", "created_at", "updated_at")
+
+    validate_password = make_password
 
     def validate_password(self, value):
         if len(value) > 6:
