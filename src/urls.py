8311e62283995de4ad98da508748156e6a9ccfce
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-14 00:02:49 +0100

    user routes

--- src/urls.py
+++ src/urls.py
@@ -2,8 +2,13 @@
 
 from rest_framework.routers import SimpleRouter
 
-from .services import ModuleServiceViewSet, LessonServiceViewSet
+from .services import (
+    ModuleServiceViewSet, 
+    LessonServiceViewSet, 
+    UserServiceViewSet
+    )
 
 router = SimpleRouter()
 router.register("modules", ModuleServiceViewSet)
 router.register("lessons", LessonServiceViewSet)
+router.register("users", UserServiceViewSet)
