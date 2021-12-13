adcab5131f5aed68f012072ee0cb485e1d865f07
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 23:26:48 +0100

    module model

---
+++ src/models/module/module.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Module(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'module'
+        verbose_name_plural = 'modules'
+
+        db_table = 'module'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
