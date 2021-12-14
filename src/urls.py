c9de5ab705dcb60889c1bb73b972b78065879c6e
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-14 20:19:11 +0100

    add auth router

--- src/urls.py
+++ src/urls.py
@@ -1,14 +1,16 @@
-# from django.urls import path
+from django.urls import path
 
 from rest_framework.routers import SimpleRouter
 
-from .services import (
-    ModuleServiceViewSet, 
-    LessonServiceViewSet, 
-    UserServiceViewSet
-    )
+from .services import ModuleServiceViewSet, LessonServiceViewSet, UserServiceViewSet
+
+from .services.user_service import UserLoginAPIView
 
 router = SimpleRouter()
 router.register("modules", ModuleServiceViewSet)
 router.register("lessons", LessonServiceViewSet)
 router.register("users", UserServiceViewSet)
+
+urlpatterns = [
+    path("user/login/", UserLoginAPIView.as_view()),
+]
