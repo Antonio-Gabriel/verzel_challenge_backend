8220f524d8375da71e8fe45919fe6b98cc36a451
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-14 22:59:42 +0100

    implement unique module

--- src/models/module/module.py
+++ src/models/module/module.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 class Module(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
 
     class Meta:
-        verbose_name = 'module'
-        verbose_name_plural = 'modules'
+        verbose_name = "module"
+        verbose_name_plural = "modules"
 
-        db_table = 'module'
-        ordering = ['name']
+        db_table = "module"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
