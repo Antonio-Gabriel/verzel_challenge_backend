2b18a382bb83c23f1271430d9d99da5db26ebe07
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-14 20:31:18 +0100

    desactive authentication with session

--- core/settings.py
+++ core/settings.py
@@ -125,8 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',         
+    'DEFAULT_AUTHENTICATION_CLASSES': (             
         'src.authentication.backend.JWTAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',        
     )
