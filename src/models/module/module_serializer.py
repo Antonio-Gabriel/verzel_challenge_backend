1e54beddf67674fa6c7a35df8d50835aabe8bd97
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-15 18:41:38 +0100

    alter hyperlinked relared field to nested relashion ship

--- src/models/module/module_serializer.py
+++ src/models/module/module_serializer.py
@@ -2,14 +2,18 @@ from .module import Module
 
 from rest_framework import serializers
 
+from ..lessons.lesson_serializer import LessonSerializer
+
 
 class ModuleSerializer(serializers.ModelSerializer):
     """Module Serializer"""
 
     # HyperLinked Related Field
-    lessons = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name="lesson-detail"
-    )
+    # lessons = serializers.HyperlinkedRelatedField(
+    #     many=True, read_only=True, view_name="lesson-detail"
+    # )
+
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Module
